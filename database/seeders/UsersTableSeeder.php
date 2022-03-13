a27ef58e2deb574487c200cda624b2d415ee4a6f
AndreyKisel-prog <undrokisel@gmail.com>
2022-03-13 10:55:32 +0300

    created models: BlogCategory, BlogPost ++ created Factory: BlogPost ++ created seeders: BlogCategory,Users + migrations blog_categories + blog_post

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' =>'author is unknown',
+                'email' => 'author_unknown@g.g',
+                'password' => Hash::make(Str::random(16) ),
+            ], [
+                'name' =>'author',
+                'email' => 'author1@g.g',
+                'password' => Hash::make('123123' ),
+            ]
+        ];
+        DB::table('users')->insert($data);
+
+    }
+}
